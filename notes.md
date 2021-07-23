38bd955057bfa4209cb83edaf4fde4603424da82
raketenlurch <raketenlurch@riseup.net>
2021-07-23 02:18:08 +0200

    Initial commit

---
+++ notes.md
@@ -0,0 +1,4 @@
+# Libs 
+- [mathplotlib](https://matplotlib.org/)
+- [plotly](https://plotly.com/python/)
+- [Beispiele](https://www.python-graph-gallery.com/violin-plot/)
---
+++ parseJson.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen 
+import urllib.parse
+import json
+
+def parse_json_from_website(pegel_location):
+    with open("data.txt", "w") as data:
+        url = "www.pegelonline.wsv.de/webservices/rest-api/v2/stations/" + pegel_location + "/W/measurements.json"
+        encoded_url = urllib.parse.quote(url)
+        
+        if not urllib.parse.urlparse(encoded_url).scheme:
+            encoded_url = "https://" + encoded_url
+        
+        response = urlopen(encoded_url)
+        
+        data_json = json.loads(response.read())
+        
+        print(json.dumps(data_json, indent=4, sort_keys=True))
+        
+parse_json_from_website("Köln")
