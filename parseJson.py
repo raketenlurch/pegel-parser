092ba938464d03be1743a6fc489ff09c33ca758a
raketenlurch <raketenlurch@riseup.net>
2021-07-25 21:55:34 +0200

    Add download_pegel_locations functions

--- parseJson.py
+++ parseJson.py
@@ -2,8 +2,26 @@ from urllib.request import urlopen
 import urllib.parse
 import json
 
+def download_pegel_locations():
+    with open("pegel_location.json", "w") as data:
+        url = "www.pegelonline.wsv.de/webservices/rest-api/v2/stations.json"
+        encoded_url = urllib.parse.quote(url)
+        
+        if not urllib.parse.urlparse(encoded_url).scheme:
+            encoded_url = "https://" + encoded_url
+            
+        response = urlopen(encoded_url)
+        
+        data_json = json.loads(response.read())
+        
+        beautified_json = json.dumps(data_json, indent=4, sort_keys=True)
+        
+        data.write(beautified_json)
+        
+        print("Anzahl der Messstationen:", len(data_json))
+
 def parse_json_from_website(pegel_location):
-    with open("data.txt", "w") as data:
+    with open("data.json", "w") as data:
         url = "www.pegelonline.wsv.de/webservices/rest-api/v2/stations/" + pegel_location + "/W/measurements.json"
         encoded_url = urllib.parse.quote(url)
         
@@ -20,6 +38,10 @@ def parse_json_from_website(pegel_location):
         
         print(beautified_json)
 
+def extract_values_from_json(file):
+    pass
+
 # user input
-location = input("Bitte den Ort des gewünschten Pegels eingeben: ")
-parse_json_from_website(location)
+show_pegel_locations()
+#location = input("Bitte den Ort des gewünschten Pegels eingeben: ")
+#parse_json_from_website(location)
